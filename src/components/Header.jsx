60fc07a0a99e4a9f0d6ab9003f293a10659783ad
Ben Lee <benjamintlee600@gmail.com>
2020-10-26 12:16:28 +0000

    created basic header with minimal styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+  
+    <header className="App-header">
+      <h1>Rainy Day</h1>
+    <div className="sub-heading">
+      <h3>Enter your email address and city for email notifications on rainy days</h3>
+    </div>
+    </header>
+
+  )
+}
+export default Header
