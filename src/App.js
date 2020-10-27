7df97cb68f4df671f6fdcacd8e31da052130cd2e
Ben Lee <benjamintlee600@gmail.com>
2020-10-27 15:09:09 +0000

    added rain animation overlay

--- src/App.js
+++ src/App.js
@@ -2,15 +2,37 @@
 import './App.css';
 import Header from './components/Header'
 import DetailInputForm from './components/DetailInputForm'
+import ReactAnimatedWeather from 'react-animated-weather';
+
+
+ 
+const defaults = {
+  icon: 'RAIN',
+  color: '#5b677a',
+  size: '100vw',
+  animate: true
+};
+
 
 
 function App() {
   return (
+    <div>
+
+    
     <div className="App" >
-        <div id="background-image"></div>
-        <Header className="App-header" />
-        <DetailInputForm />
+      <Header className="App-header" />
+      <ReactAnimatedWeather
+        className="rain-animation"
+        icon={defaults.icon}
+        color={defaults.color}
+        size={defaults.size}
+        animate={defaults.animate}/>
+      <DetailInputForm />
     </div>
+    </div>
+      
+
   );
 }
 
